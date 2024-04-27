75cb8165755e5026a7ebd211147873cd1ef66ac9
Rajiv kumar <rajivdhanama00@gmail.comuser.name>
2024-04-28 01:01:52 +0530

    age calculator adding

--- main.js
+++ main.js
@@ -41,10 +41,10 @@ ScrollReveal({
     delay:200,
 });
 
-ScrollReveal().reveal('.home-content, heading', { origin: 'top'});
-ScrollReveal().reveal('.home-img, .service-container, .portfolio-box, .contact form', { origin: 'button'});
-ScrollReveal().reveal('.home-contact h1, .about-img', { origin: 'left'});
-ScrollReveal().reveal('.home-contact p .about-content', { origin: 'right'});
+// ScrollReveal().reveal('.home-content, heading', { origin: 'top'});
+// ScrollReveal().reveal('.home-img, .service-container, .portfolio-box, .contact form', { origin: 'button'});
+// ScrollReveal().reveal('.home-contact h1, .about-img', { origin: 'left'});
+// ScrollReveal().reveal('.home-contact p .about-content', { origin: 'right'});
 
 
 // //===================  typed js =========================
